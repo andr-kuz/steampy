fea23f73d804ea9b5ed13996ea3beed2e97a9901
andrey <awakum192@gmail.com>
2023-11-14 05:19:14 +0300

    Fix: typehinting

--- steampy/_utils.py
+++ steampy/_utils.py
@@ -1,16 +1,7 @@
 from bs4 import BeautifulSoup
 import json
 import re
-from typing import TypedDict
-
-class ProductDataTypeHint(TypedDict):
-    market_hash_name: str
-    item_nameid: str
-    app_id: str
-    sales: list
-    market_ban: int
-    context_id: str
-    currency: str
+from typing import NotRequired, TypedDict
 
 class ProductHistogramTypeHint(TypedDict):
     success: int
@@ -27,6 +18,16 @@ class ProductHistogramTypeHint(TypedDict):
     graph_max_x: float
     price_prefix: str
     price_suffix: str
+
+class ProductDataTypeHint(TypedDict):
+    market_hash_name: str
+    item_nameid: str
+    app_id: str
+    sales: list
+    market_ban: int
+    context_id: str
+    currency: str
+    histogram: NotRequired[ProductHistogramTypeHint]
 
 def extract_product_data(html: str) -> ProductDataTypeHint:
     context_id = '0'
